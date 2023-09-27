ecebffae0501ea2593de365df205e1c711cb1f81
khadijaludhi <khadijaludhi@gmail.com>
2023-09-27 22:05:23 +0100

    this code presents the user with 10 random arithmetic problems. A nested while loop starts and it continues until the user gives the correct answer.

--- challenge.py
+++ challenge.py
@@ -3,6 +3,7 @@ import random
 OPERATORS = ["+", "-", "/", "*"]
 MIN_OPERAND = 3
 MAX_OPERAND = 12
+TOTAL_PROBLEMS = 10
 
 def generate_problem():
     left = random.randint(MIN_OPERAND, MAX_OPERAND)
@@ -12,6 +13,10 @@ def generate_problem():
     expr = str(left) + " " + operator + " " + str(right)
     answer = eval(expr)
     return expr, answer
-
-expr, answer = generate_problem()
-print(expr, answer)
+2
+for i in range(TOTAL_PROBLEMS):
+    expr, answer = generate_problem()
+    while True:
+        guess = input("Problem #" + str(i + 1) + ": " + expr + " = ")
+        if guess == str(answer):
+            break
